f17cb25f2e9fa9343c3041b20a9ddb7afc7bfe2a
MagmaZ3637 <zidanalfa18@gmail.com>
2025-11-14 19:51:03 +0700

    api resource added

---
+++ app/Http/Controllers/api/Auth/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use App\Models\Guru;
+use App\Models\Siswa;
+use Illuminate\Support\Facades\Hash;
+use \Illuminate\Validation\ValidationException;
+
+class AuthController
+{
+    public function login(Request $request)
+    {
+        if (!$request->role || !$request->username || !$request->password) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Informasi tidak lengkap'
+            ], 400);
+        }
+
+        if (!in_array($request->role, ['admin', 'guru', 'siswa'])) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Role tidak valid'
+            ], 400);
+        }
+
+        switch ($request->role) {
+            case 'admin':
+                $user = Admin::where('username', $request->username)->first();
+                $userIdField = 'id_admin';
+                break;
+
+            case 'guru':
+                $user = Guru::where('nip', $request->username)->first();
+                $userIdField = 'id_guru';
+                break;
+
+            case 'siswa':
+                $user = Siswa::where('nis', $request->username)->first();
+                $userIdField = 'id_siswa';
+                break;
+        }
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Akun tidak ditemukan'
+            ], 404);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Akun atau password salah'
+            ], 401);
+        }
+
+        $tokenResult = $user->createToken('auth_token');
+        $token = $tokenResult->plainTextToken;
+
+        $tokenResult->accessToken->expires_at = now()->addDays(7); // 7 hari
+        $tokenResult->accessToken->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Login berhasil',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_at' => '7 hari',
+            'id_user' => $user->{$userIdField},
+            'nama' => $user->nama,
+            'username' => $request->username,
+            'role' => $request->role,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logout berhasil']);
+    }
+}
